e4d6df28015e2c681345987103436cd61a05fab4
Bonamente <56506035@protonmail.com>
2021-10-29 09:20:53 +0500

    refactor(01-read-file): replace console.log to stdout.write

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const stdout = process.stdout;
 
 const filePath = path.join(__dirname, 'text.txt');
 let fileData = '';
@@ -7,5 +8,5 @@ let fileData = '';
 const stream = fs.createReadStream(filePath, 'utf-8');
 
 stream.on('data', (partData) => fileData += partData);
-stream.on('end', () => console.log(fileData));
+stream.on('end', () => stdout.write(fileData));
 stream.on('error', (error) => console.log('Error', error.message));
